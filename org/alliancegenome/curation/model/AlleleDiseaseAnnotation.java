1d23624d33af6f6b2ebb7e70d60f04c262825f60
GitHub Action <action@github.com>
2021-10-28 15:37:14 +0000

    Updating generated outputs

--- org/alliancegenome/curation/model/AlleleDiseaseAnnotation.java
+++ org/alliancegenome/curation/model/AlleleDiseaseAnnotation.java
@@ -3,7 +3,9 @@ package org.alliancegenome.curation.model;
 import java.util.List;
 import lombok.*;
 
-
+/**
+  An annotation asserting an association between an allele and a disease supported by evidence.
+**/
 @Data
 @EqualsAndHashCode(callSuper=false)
 public class AlleleDiseaseAnnotation extends DiseaseAnnotation {
--- org/alliancegenome/curation/model/GeneDiseaseAnnotation.java
+++ org/alliancegenome/curation/model/GeneDiseaseAnnotation.java
@@ -3,7 +3,9 @@ package org.alliancegenome.curation.model;
 import java.util.List;
 import lombok.*;
 
-
+/**
+  An annotation asserting an association between a gene and a disease supported by evidence.
+**/
 @Data
 @EqualsAndHashCode(callSuper=false)
 public class GeneDiseaseAnnotation extends DiseaseAnnotation {
