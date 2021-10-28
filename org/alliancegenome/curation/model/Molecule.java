3d509851c7b14f91e85eb47fe61e993aee14ec60
GitHub Action <action@github.com>
2021-10-28 18:21:42 +0000

    Updating generated outputs

---
+++ org/alliancegenome/curation/model/Molecule.java
@@ -0,0 +1,23 @@
+package org.alliancegenome.curation.model;
+
+import java.util.List;
+import lombok.*;
+
+/**
+  Molecules as described by WormBase
+**/
+@Data
+@EqualsAndHashCode(callSuper=false)
+public class Molecule  {
+
+  private String curie;
+  private String name;
+  private String inchi;
+  private String inchiKey;
+  private String iupac;
+  private String formula;
+  private String smiles;
+  private List<Synonym> synonyms;
+  private List<CrossReference> crossReferences;
+
+}
