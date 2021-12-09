e324470962b7ef221f03cc61f8a73966430f532d
GitHub Action <action@github.com>
2021-12-09 20:37:45 +0000

    Updating generated outputs

--- org/alliancegenome/curation/model/Resource.java
+++ org/alliancegenome/curation/model/Resource.java
@@ -9,5 +9,16 @@ import lombok.*;
 public class Resource extends InformationContentEntity {
 
   private String title;
+  private String isoAbbreviation;
+  private String medlineAbbreviation;
+  private String copyrightDate;
+  private String printIssn;
+  private String onlineIssn;
+  private InformationContentEntity publisher;
+  private String volumes;
+  private String summary;
+  private List<Synonym> synonyms;
+  private List<AuthorReference> authors;
+  private List<AuthorReference> editors;
 
 }
